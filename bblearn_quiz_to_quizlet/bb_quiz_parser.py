00208a137918f86820925105cd109eb3a0d9be58
ber98 <ber98@nau.edu>
2020-05-05 13:51:10 -0700

    took out prints

--- bblearn_quiz_to_quizlet/bb_quiz_parser.py
+++ bblearn_quiz_to_quizlet/bb_quiz_parser.py
@@ -7,18 +7,14 @@ with open(sys.argv[1], 'r') as file:
 quiz.replace(",","_").replace(";",":")
 questions = filter(None,quiz.split("Question"))
 qnaStr = ""
+
 i = 0
 for question in questions:
     m = re.search('[A-Z]',question)
-    print(i)
-    if m:
-        print(question[m.start():])
     qnaLst = question[m.start():].split("\n")
-    print("ANSWER LIST: ")
-    print(qnaLst)
     qnaStr = qnaStr + qnaLst[0].strip();
-    print("CORRECT ANSWER")
     answerLst = []
+
     j = 0
     for answer in qnaLst:
         if "Correct" in answer and len(answer) < 9:
@@ -26,8 +22,9 @@ for question in questions:
         elif "Correct" in answer:
             answerLst.append(answer.split("Correct")[1])
         j = j + 1
-    print("HELLO THERE ___________ " + answerLst[0])
+
     qnaStr += "," + answerLst[0].strip() + ";"
-    print('\n\n')
     i = i + 1
-print("FINAL STRING: " + qnaStr)
+
+with open('qna.txt', "w") as qna:
+    qna.write(qnaStr)
