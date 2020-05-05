3f58ee96d2dcd5ff9708fa860ba27a07159a4434
ber98 <ber98@nau.edu>
2020-05-05 15:25:20 -0700

    minor edits to make it perform better

--- bblearn_quiz_to_quizlet/bb_quiz_parser.py
+++ bblearn_quiz_to_quizlet/bb_quiz_parser.py
@@ -4,17 +4,32 @@ import re
 with open(sys.argv[1], 'r') as file:
     quiz = file.read()
 
-quiz.replace(",","_").replace(";",":")
+quiz = quiz.replace(",","#comma#").replace(";","#semicolon#")
+
 questions = filter(None,quiz.split("Question"))
 qnaStr = ""
 
-i = 0
+def addOptions(question):
+    optionsStr = ""
+    for k in range(1,len(question)):
+        if "Selected Answer" in question[k]:
+            break
+        if question[k]:
+            print(qnaLst[k])
+            optionsStr += '\n' + question[k]
+    return optionsStr
+
 for question in questions:
+    # in bblearn, the question starts with the first capital letter
     m = re.search('[A-Z]',question)
     qnaLst = question[m.start():].split("\n")
-    qnaStr = qnaStr + qnaLst[0].strip();
-    answerLst = []
 
+    qnaStr += qnaStr + qnaLst[0].strip();
+
+    # sometimes part of the question is below the initial question, this adds it
+    qnaStr += addOptions(qnaLst)
+
+    answerLst = []
     j = 0
     for answer in qnaLst:
         if "Correct" in answer and len(answer) < 9:
@@ -24,7 +39,6 @@ for question in questions:
         j = j + 1
 
     qnaStr += "," + answerLst[0].strip() + ";"
-    i = i + 1
 
-with open('qna.txt', "w") as qna:
+with open(sys.argv[2], "w") as qna:
     qna.write(qnaStr)
